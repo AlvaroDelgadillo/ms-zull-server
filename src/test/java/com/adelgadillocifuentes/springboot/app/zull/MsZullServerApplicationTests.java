7db4c694aa3a76b84c1bf8235f2761b7678a239a
Alvaro Delgadillo Cifuentes <adelgadillocifuentes@gmail.com>
2020-05-29 17:23:25 -0500

    Enable Spring Cloud Routing Zuul API Gateway

---
+++ src/test/java/com/adelgadillocifuentes/springboot/app/zull/MsZullServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adelgadillocifuentes.springboot.app.zull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsZullServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
